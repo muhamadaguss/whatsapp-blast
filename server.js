55333c6915eba05b63eb8ac6aba4b9bd4f2e03e7
muhamad agus <muhamadagus3197@gmail.com>
2025-04-21 17:01:32 +0700

    add configure dockerfile

--- server.js
+++ server.js
@@ -24,13 +24,7 @@ function createNewClient() {
     puppeteer: { headless: true },
     args: [
       '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
-      '--disable-gpu'
+      '--disable-setuid-sandbox'
     ]
   });
 
